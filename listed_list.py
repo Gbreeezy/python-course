b0e8bef168f5202e5d3a1c0f6045deb4eb9f3318
Elisha Gerhard <makko0235@gmail.com>
2020-04-06 00:37:01 -0500

    added linked list

---
+++ listed_list.py
@@ -0,0 +1,49 @@
+# Examples of linked list in python
+
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    def add_at_front(self, data):
+        """
+        Function to add a node to front
+        :param data:
+        :return:
+        """
+
+    def is_empty(self):
+        """
+        Function to check whether the list is empty
+        :return:
+        """
+
+    def add_at_end(self):
+        """
+        Function to add node to the end
+        :return:
+        """
+
+    def delete_node(self):
+        """
+        function to delete any node
+        :return:
+        """
+
+    def get_last_node(self):
+        """
+        Function to get the last node
+        :return:
+        """
+
+    def print_list(self):
+        """
+        Function to to print the list nodes
+        :return:
+        """
